b94b515d18ecc8b13ce2547fd06d77a8edf5cf90
zlc1004 <lucaszhang1001@gmail.com>
2025-10-17 11:55:04 -0700

    Port ConfuseHack from meteor-rejects - teleports around enemies in confusing patterns

--- src/main/java/net/wurstclient/hack/HackList.java
+++ src/main/java/net/wurstclient/hack/HackList.java
@@ -88,6 +88,7 @@ public final class HackList implements UpdateListener
 	public final ChestEspHack chestEspHack = new ChestEspHack();
 	public final ChorusExploitHack chorusExploitHack = new ChorusExploitHack();
 	public final ClickAuraHack clickAuraHack = new ClickAuraHack();
+	public final ConfuseHack confuseHack = new ConfuseHack();
 	public final ClickGuiHack clickGuiHack = new ClickGuiHack();
 	public final CrashChestHack crashChestHack = new CrashChestHack();
 	public final CreativeFlightHack creativeFlightHack =
